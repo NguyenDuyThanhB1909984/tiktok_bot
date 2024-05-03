11582471c1267d2b705b338cea6a8c2c1542cf63
NguyenDuyThanhB1909984 <101155685+NguyenDuyThanhB1909984@users.noreply.github.com>
2024-05-03 15:18:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 from selenium.webdriver.chrome.service import Service
 
-service = Service(executable_path=r'chromedriver.exe')
+# service = Service(executable_path=r'chromedriver.exe')
 
 options = webdriver.ChromeOptions()
 # options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -12,8 +12,8 @@ options.add_argument("--log-level=3")
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("user-data-dir=Profile")
 
-
-bot = webdriver.Chrome(service=service, options=options)
+bot = webdriver.Chrome(options=options)
+# bot = webdriver.Chrome(service=service, options=options)
 
 bot.get("https://studio.youtube.com")
 time.sleep(3)
