839061008b8805969450dc1bd3dde0843b012c27
Duy_Thanh <thanh08052001@gmail.com>
2024-05-03 11:25:39 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=r'chromedriver.exe')
+
+options = webdriver.ChromeOptions()
+# options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument("--log-level=3")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("user-data-dir=Profile")
+
+
+bot = webdriver.Chrome(service=service, options=options)
+
+bot.get("https://studio.youtube.com")
+time.sleep(3)
+bot.save_screenshot("image/image.png")
+bot.quit()
+
+
+
+
+
